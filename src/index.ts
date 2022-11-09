db5a70dd3b9cd15f8dfe96b0b5b8c1eedf183cec
Dhaniel Sales <dhanielr94@gmail.com>
2022-11-09 11:11:27 -0300

    foo

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,29 @@
-import testConsole from "@/service/testConsole"
-testConsole('Hello world!')
+import ts from 'typescript'
+export interface UserUpdateModel {
+  params: {
+    id: string
+  }
+  body: {
+    name: string
+  }
+}
+
+function getArrayFromType<T>(): string[] {
+  
+
+  ts.factory.createArrayLiteralExpression()
+
+
+  class GhostClass implements <keyof T> {}
+
+  const ghost = new GhostClass()
+
+  const result = Object.keys(ghost)
+
+  return result
+}
+
+
+console.log(getArrayFromType<UserUpdateModel>())
+
+
